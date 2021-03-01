ac5c342e81f7d353239abfaf1e18bedddbceb07a
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-03-01 19:56:09 +0100

    Add project files.

---
+++ PracticeItAll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticeItAll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
